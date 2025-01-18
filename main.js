f20904b284c4232e68351641b656586f40e1a559
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-18 16:39:11 +0200

    Add input even listener for '#formEmail' element

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 function btnSubmitEventHandler(event){
-  console.log(event);
   event.preventDefault();
   let form = document.querySelector("form");
   if(form.reportValidity()){
@@ -8,9 +7,22 @@ function btnSubmitEventHandler(event){
   }
 }
 
+function formEmailEventListener(event){
+  let formEmailMsg = document.querySelector("#formEmailMsg");
+  if(!event.target.checkValidity()){
+    formEmailMsg.textContent = "Email needs to be in format 'email@example.com'"
+  }
+  else{
+    formEmailMsg.textContent = "";
+  }
+}
+
 function main(){
   let btnSubmit = document.querySelector("#btnSubmit");
   btnSubmit.addEventListener("click", btnSubmitEventHandler);
+
+  let formEmail = document.querySelector("#formEmail");
+  formEmail.addEventListener("input", formEmailEventListener);
 }
 
 main();
