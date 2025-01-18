ab8963a7b3360ead42c579c35e8f50414d1da455
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-18 17:20:01 +0200

    Add input event listener for '#formCountry' element

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-
-function btnSubmitEventHandler(event){
+function btnSubmitEventHandler(event) {
   event.preventDefault();
   let form = document.querySelector("form");
-  if(form.reportValidity()){
+  if (form.reportValidity()) {
     alert("Form submitted");
   }
 }
 
-function formEmailEventListener(event){
+function formEmailEventListener(event) {
   let formEmailMsg = document.querySelector("#formEmailMsg");
-  if(!event.target.checkValidity()){
-    formEmailMsg.textContent = "Email needs to be in format 'email@example.com'"
-  }
-  else{
+  if (!event.target.checkValidity()) {
+    formEmailMsg.textContent =
+      "Email needs to be in format 'email@example.com'";
+  } else {
     formEmailMsg.textContent = "";
   }
 }
 
-function main(){
+function formCountryEventListener(event) {
+  let formCountryMsg = document.querySelector("#formCountryMsg");
+  if (!event.target.checkValidity()) {
+    formCountryMsg.textContent = "Please enter a country name";
+  } else {
+    formCountryMsg.textContent = "";
+  }
+}
+
+function main() {
   let btnSubmit = document.querySelector("#btnSubmit");
   btnSubmit.addEventListener("click", btnSubmitEventHandler);
 
   let formEmail = document.querySelector("#formEmail");
   formEmail.addEventListener("input", formEmailEventListener);
+
+  let formCountry = document.querySelector("#formCountry");
+  formCountry.addEventListener("input", formCountryEventListener);
 }
 
 main();
